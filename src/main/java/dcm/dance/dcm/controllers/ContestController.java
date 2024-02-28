8499704f0372d2d4ad879838b131396078d977f1
iDariusSerban <idariusserban@gmail.com>
2024-02-29 00:56:20 +0200

    Add contest merge

--- src/main/java/dcm/dance/dcm/controllers/ContestController.java
+++ src/main/java/dcm/dance/dcm/controllers/ContestController.java
@@ -26,13 +26,7 @@ public class ContestController {
 
     @PostMapping("/add")
     public ResponseEntity<Contest> addContest(@RequestBody ContestRequestDTO contestRequestDTO) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(contestService.addContest(contestRequestDTO));
-        } catch (Exception e) {
-            // Adaugă un logging pentru a afișa detalii despre excepție
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-       // return ResponseEntity.status(HttpStatus.CREATED).body(contestService.addContest(contestRequestDTO));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(contestService.addContest(contestRequestDTO));
     }
 }
